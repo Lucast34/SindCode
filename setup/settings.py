b6d192afd9767bf6d475d5220c067e3350a35c64
Lucast34 <lucast34t@gmail.com>
2025-10-21 11:44:44 -0300

    Update setup

--- setup/settings.py
+++ setup/settings.py
@@ -41,7 +41,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'noticias'
+    'noticias',
+    'associados'
 ]
 
 MIDDLEWARE = [
