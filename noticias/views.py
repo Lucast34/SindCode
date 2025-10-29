1fb327e261cd4c17c73947607f1e535454f15de3
Lucast34 <lucast34t@gmail.com>
2025-10-29 11:50:18 -0300

    Update views

--- noticias/views.py
+++ noticias/views.py
@@ -1,11 +1,28 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
 # funcao
 # se def dentro classe = metodo
 # se def fora classe = função
 
 # Create your views here.
 
-def index(request ):
-    return render(request,'noticias/index.html')
+def index(request):
+    # definindo um mock com dict
+    dados = {
+
+        1: {"titulo": "Mulheres dev",
+            "conteudo": "Mulheres programadoras em python",
+            "data_publicacao": "29/10/2025"
+        },
+        2: {"titulo": "Programadores kids",
+            "conteudo": "Programadoras em python no dia das crianças",
+            "data_publicacao": "12/10/2025"
+        }
+    }
+
+
+    return render(request,
+    'noticias/index.html',
+          {'cards':dados})
