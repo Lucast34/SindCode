0bb903dc54efec6490df8213a080bb46711c6b98
Lucast34 <lucast34t@gmail.com>
2025-10-30 11:48:14 -0300

    Update models.py, migrations with django

--- noticias/models.py
+++ noticias/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+# Estudar o que é orm (object relation mapper)
+
+#Herença é uma classe herda de outra classe e implementa suas próprias caracteristicas
+#Filho(Pai) utilizo o sinal de () para herença
+
+class Categoria(models.Model):# a classe é um conjunto de objetos
+
+    # toda classe começa com letra Maiscula(PascalCase)
+    nome= models.CharField(max_length=88, null=False, blank=False)
+
+    #max_length: limitr de caracteres
+    #null-> nenhuma informação
+    #blank:string vazia""
+
+    def __str__(self):
+        return f"Categoria [nome={self.nome}]"
