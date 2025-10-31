7d60a3c836437988c9e3caa0d41122933f318b9f
Lucast34 <lucast34t@gmail.com>
2025-10-31 11:48:03 -0300

    Update model.py to create new objects

--- noticias/models.py
+++ noticias/models.py
@@ -17,3 +17,25 @@ class Categoria(models.Model):# a classe é um conjunto de objetos
 
     def __str__(self):
         return f"Categoria [nome={self.nome}]"
+
+# Django ORM
+# Python e Flask: SQLAlchemy
+# Python e Django: Django ORM
+# Java: Hibernate
+# C#: Entity Framework
+# PHP: Doctrine
+
+# vantagem:
+
+"""
+    - Usar a linguagem de fluência
+    - Migração (migrar de um banco MySQL para um SQLServer)
+    - Menos erros (conexão, segurança entre outros)
+    - Toolkit extra interação com banco de dados
+"""
+# desvantagem:
+
+"""
+    - Sistemas legados poder ser um problema
+    - Iniciantes criar uma ilusão (não precisa estudar SQL)
+"""
