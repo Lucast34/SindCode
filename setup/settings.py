bf8b4ff95ed2844d860bc20d0312efeb363d8e25
Lucast34 <lucast34t@gmail.com>
2025-10-31 11:49:47 -0300

    Update settings.py, for new database

--- setup/settings.py
+++ setup/settings.py
@@ -43,8 +43,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'noticias',
-    'associados'
+    'noticias.apps.NoticiasConfig',
+    'associados.apps.AssociadosConfig'
 ]
 
 MIDDLEWARE = [
@@ -83,8 +83,12 @@ WSGI_APPLICATION = 'setup.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'sindcode_full',
+        'USER':'root',
+        'PASSWORD':'',
+        'HOST':'localhost',
+        'PORT':'3306'
     }
 }
 
