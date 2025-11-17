9eb7302171d668988a688fc2572068d8697df23b
Lucast34 <lucast34t@gmail.com>
2025-11-17 11:23:45 -0300

    Update urls and views | Add Buscar function

--- noticias/urls.py
+++ noticias/urls.py
@@ -3,9 +3,11 @@ from django.urls import path
 from django.conf.urls.static import static
 from noticias.views import index
 from noticias.views import autores
+from noticias.views import buscar
 from setup.settings import MEDIA_ROOT
 
 urlpatterns = [
     path('',index),
-    path('autor/',autores)
+    path('autor/',autores),
+    path("buscar/",buscar, name="buscar")
 ]+static(settings.MEDIA_URL,document_root= settings.MEDIA_ROOT)
