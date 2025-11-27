d83ce28c1f81dca399a4c58fefc32fccfbe8f248
Lucast34 <lucast34t@gmail.com>
2025-11-27 11:44:53 -0300

    Update associados
    Update urls annd views for 'associados'

--- associados/urls.py
+++ associados/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from associados.views import associados
+from associados.views import associados,login,cadastro
 
 urlpatterns = [
-    path('associados', associados ,name='cadastro'),
+    path('associados', associados ,name='associados'),
+    path('cadastro', cadastro ,name='cadastro'),
+    path('login', login ,name='login')
 ]
--- associados/views.py
+++ associados/views.py
@@ -3,3 +3,8 @@ from django.shortcuts import render
 # Create your views here.
 def associados(request ):
     return render(request,'associados/index.html')
+def login(request ):
+    return render(request,'associados/login.html')
+def cadastro(request ):
+    return render(request,'associados/cadastro.html')
+
