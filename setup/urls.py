77765358bf621643695d728c90295803f94cb783
Lucast34 <lucast34t@gmail.com>
2025-11-27 11:47:45 -0300

    Update setup/
    Update url for associado

--- setup/urls.py
+++ setup/urls.py
@@ -20,5 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('noticias.urls')),
-    path('index',include('associados.urls'),name="cadassociados")
+    path('',include('associados.urls'),name="cadassociados")
 ]
