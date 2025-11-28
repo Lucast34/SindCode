f4ae135d72b6be5f2fc5c0c5d3840e53fc166e1b
Lucast34 <lucast34t@gmail.com>
2025-11-28 11:19:27 -0300

    Update associados/views.py
    Defined cadastro with forms to register new users

--- associados/views.py
+++ associados/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
-
+from associados.forms import AssociadoForms
 # Create your views here.
 def associados(request ):
     return render(request,'associados/index.html')
 def login(request ):
     return render(request,'associados/login.html')
 def cadastro(request ):
-    return render(request,'associados/cadastro.html')
+    form = AssociadoForms()
+    return render(request,'associados/cadastro.html', {'form':form})
 
