924c305d16fa4ca73dde7b759e92a874a4baa13f
Lucast34 <lucast34t@gmail.com>
2025-12-02 11:37:27 -0300

    Update forms
    new fields for forms and new authenticator

--- associados/forms.py
+++ associados/forms.py
@@ -22,10 +22,28 @@ class AssociadoForms(forms.Form):
         max_length=100
     )
     
+    email = forms.EmailField(
+        label="E-mail",
+        required=True,
+        max_length=100,
+        widget=forms.EmailInput(attrs={'class': 'form-control',
+                                       'placeholder': 'Digite seu e-mail'})
+    )
+    
+    rg = forms.CharField(
+        label="RG",
+        required=True,
+        max_length=7,
+        widget=forms.TextInput(attrs={
+            'class': 'form-control',
+            'placeholder': 'Digite seu RG'})
+    )
+    
+    
     genero= forms.ChoiceField(
         label='Por favor informe o seu gênero',
         choices=GENERO_CHOICE,
-        widget=forms.Select(attrs={'class': 'form-control'}),
+        widget=forms.Select(attrs={'class': 'form-control','placeholder': 'Identifique sua identidade'}),
         required=True  # Permitir que a pessoa não declare, embora haja 'Prefiro Não Declarar'
     )
     
@@ -36,13 +54,40 @@ class AssociadoForms(forms.Form):
         help_text='Use apenas se a opção "Outro" tiver sido selecionada.'
     )
      
-    senha= forms.CharField(
-        label='senha',
+    senha_1 = forms.CharField(
+        label='Senha',
         required=True,
         max_length=70,
-        widget=forms.PasswordInput(attrs={'class':'form-control'})
-    ) 
+        widget=forms.PasswordInput(
+            attrs={'class': 'form-control',
+                'placeholder': 'Digite sua senha'}
+        ),
+    )
+    senha_2 = forms.CharField(
+        label='Senha',
+        required=True,
+        max_length=70,
+        widget=forms.PasswordInput(
+        attrs={
+                'class': 'form-control',
+                'placeholder': 'Digite sua senha novamente',   }
+        )
+    )
     
-    
-    
-    
+
+
+
+
+class LoginForm(forms.Form):
+
+    nome_login = forms.CharField(
+        label='Nome de Login',
+        required=True,
+        max_length=100
+    )
+    senha = forms.CharField(
+        label='Senha',
+        required=True,
+        max_length=70,
+    widget = forms.PasswordInput()
+    )
--- associados/views.py
+++ associados/views.py
@@ -1,11 +1,38 @@
-from django.shortcuts import render
-from associados.forms import AssociadoForms
-# Create your views here.
-def associados(request ):
-    return render(request,'associados/index.html')
-def login(request ):
-    return render(request,'associados/login.html')
-def cadastro(request ):
-    form = AssociadoForms()
-    return render(request,'associados/cadastro.html', {'form':form})
+from django.shortcuts import render, redirect
+from associados.forms import AssociadoForms, LoginForm
+from django.contrib.auth.models import User
 
+
+def associados(request):
+    return render(request, 'associados/index.html')
+
+def login(request):
+    form = LoginForm()
+    return render(request, 'associados/login.html', {'form': form})
+
+def cadastro(request):
+    form = AssociadoForms()
+    if request.method == 'POST':
+        form = AssociadoForms(request.POST)
+        if form['senha_1'].value() != form['senha_2'].value():
+            return redirect('cadastro')
+        
+        nome_completo = form['nome_completo'].value()
+        nome_social = form['nome_social'].value()
+        genero = form['genero'].value()
+        email = form['email'].value()
+        senha = form['senha'].value()
+        
+        if User.objects.filter(username=nome_completo).exists():
+            return redirect('cadastro')
+        
+        associado = User.objects.create_user(
+            username=nome_completo,
+            email= email,
+            password=senha
+        )
+        associado.save()
+        
+        return redirect('login')
+        
+    return render(request, 'associados/cadastro.html',{'form': form})
